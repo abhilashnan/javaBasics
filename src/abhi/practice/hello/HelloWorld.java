257f8a870b820f8a36e292a658ad92bd56ecb030
AbhilashNani <naniabhilash22@gmail.com>
2022-05-31 12:27:04 +0530

    first Thing

---
+++ src/abhi/practice/hello/HelloWorld.java
@@ -0,0 +1,13 @@
+package abhi.practice.hello;
+
+public class HelloWorld {
+    //we are
+    static void add(int a ,int b){
+        System.out.println(a+b);
+    }
+    public static void main(String[] args) {
+        add(5,7);
+        System.out.println("hello World");
+    }
+
+}
